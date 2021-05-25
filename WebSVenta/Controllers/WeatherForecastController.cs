36620400274bb5426a8863d0848329f82ed47eed
OSCAR ALEXANDER CASTRO SALAS <alexcaxtro@gmail.com>
2021-05-24 21:28:21 -0400

    Agregar archivos de proyecto.

---
+++ WebSVenta/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSVenta.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+      
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            List<WeatherForecast> lst = new List<WeatherForecast>();
+            lst.Add(new WeatherForecast() { Id=5, Nombre="Oscar"});
+            lst.Add(new WeatherForecast() { Id = 6, Nombre = "Alexander" });
+            return lst;
+        }
+    }
+}
---
+++ WebSVenta/WeatherForecast.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace WebSVenta
+{
+    public class WeatherForecast
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
